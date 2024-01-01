76eadbe512de74be413c2aeee2052e655b34fe84
uchikosi yuuki <ucchikohiwithsugar@gmail.com>
2024-01-01 15:37:53 +0900

    コメント挿入

--- src/Capsule.java
+++ src/Capsule.java
@@ -1,13 +1,18 @@
 
 public class Capsule {
     public static void main(String[] args) {
+
         Person taro = new Person("山田太郎", 20);
         System.out.println(taro.getName());
-        taro.setName("花子");
-        System.out.println(taro.getName( ));
-        
-        
         System.out.println(taro.getAge());
+
+        taro.setName("花子");
+//        personeクラスのpublic void setName(String name) {this.name = name;}
+//        のコードのthis.name部分に花子がsetされる
+        System.out.println(taro.getName( ));
+//        getName(get の後は大 文字)に変更することで読み取る(name を get)ことが出来るようになります。
+//        Personクラスのpublic String getName() {return this.name;}を読み取ってgetする
+//        setで変更された変数の花子が表示される
         taro.setAge(30);
         System.out.println(taro.getAge());
     }
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,8 @@
 public class Person {
     private String name = null;
     private int age = 0;
-
+//  privateに変更したことでアクセスの許可範囲がpublicの全てクラスから
+//    自分のクラスのみなる
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
@@ -9,10 +10,17 @@ public class Person {
 
     public String getName() {
         return this.name;
+//        get は値を取得すること。
+//        getの後の1文字は大文字になる。 getの時は必ずreturnします。
+//        今回だと getName でフィールドの値の取得を して呼び出し元に返します。
     }
 
     public void setName(String name) {
         this.name = name;
+//        set は登録すること。
+//        setの後の1文字も大文字になるので注意。
+//        set の時は戻り値の型の位置に voidと書きます。
+//        今回だとsetNameで名前を書き換える「処理」を行うだけなので戻り値なしの void になる。
     }
 
     public int getAge() {
@@ -23,3 +31,4 @@ public class Person {
         this.age = age;
     }
 }
+//3  カプセル化・・・隠しておきたい値(データ)の隠蔽をすること。そのなかで、必要な 情報を書き込んだり取得したりすることが出来きます。  
